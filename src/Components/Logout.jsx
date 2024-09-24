e71f368ed5e8c2ed9ebd042c863da3e90bc5069f
Oluwadare Oluwatobiloba <112389225+tobidechamp15@users.noreply.github.com>
2024-09-24 17:16:14 -0500

    log out

--- src/Components/Logout.jsx
+++ src/Components/Logout.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { faSignOut } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getAuth, signOut } from "firebase/auth"; // Import Firebase auth
 
 const Logout = () => {
-  const handleLogOut = () => {};
+  const handleLogOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful, you can redirect the user
+        console.log("Logged out successfully");
+      })
+      .catch((error) => {
+        // Handle error if sign-out fails
+        console.error("Logout error: ", error);
+      });
+  };
+
   return (
     <div
       onClick={handleLogOut}
-      className="flex justify-center  text-xs text-[14px] bg-red-600 items-center  py-2 px-4 rounded-lg btn hover:bg-red-700 transition-all ease-in-out text-white"
+      className="flex justify-center text-xs text-[14px] bg-red-600 items-center py-2 px-4 rounded-lg btn hover:bg-red-700 transition-all ease-in-out text-white"
     >
       <FontAwesomeIcon icon={faSignOut} className="me-2 text-[14px]" />
       Logout
