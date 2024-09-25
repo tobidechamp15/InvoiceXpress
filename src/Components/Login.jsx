c72f7089287172c2edbea9c77c68561d5991a3a8
Oluwadare Oluwatobiloba <112389225+tobidechamp15@users.noreply.github.com>
2024-09-25 15:09:18 -0500

    signInWithGoogle

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,6 +11,7 @@ import {
 import { app, db } from "./firebase/config";
 import { doc, getDoc } from "firebase/firestore";
 import { useUser } from "../context/UserContext";
+import SignInWithGoogle from "./SignInWithGoogle";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -73,31 +74,31 @@ const Login = () => {
     }
   };
 
-  const handleSignInWithGoogle = async () => {
-    setLoading(true); // Show loader for Google sign-in
+  // const handleSignInWithGoogle = async () => {
+  //   setLoading(true); // Show loader for Google sign-in
 
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+  //   const auth = getAuth();
+  //   const provider = new GoogleAuthProvider();
 
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      const loggedInUserId = user.uid;
-      localStorage.setItem("userId", loggedInUserId);
-      console.log(user);
-      saveUser(user);
-      // navigate("/dashboard/information");
-      console.log(token);
-      navigate("/dashboard/information");
-    } catch (error) {
-      console.error(error);
-      setError(true);
-    } finally {
-      setLoading(false); // Stop loading after process
-    }
-  };
+  //   try {
+  //     const result = await signInWithPopup(auth, provider);
+  //     const credential = GoogleAuthProvider.credentialFromResult(result);
+  //     const token = credential.accessToken;
+  //     const user = result.user;
+  //     const loggedInUserId = user.uid;
+  //     localStorage.setItem("userId", loggedInUserId);
+  //     console.log(user);
+  //     saveUser(user);
+  //     // navigate("/dashboard/information");
+  //     console.log(token);
+  //     navigate("/dashboard/information");
+  //   } catch (error) {
+  //     console.error(error);
+  //     setError(true);
+  //   } finally {
+  //     setLoading(false); // Stop loading after process
+  //   }
+  // };
 
   return (
     <div className="relative flex w-full xsm:min-h-screen bg-white text-gray-800">
@@ -164,13 +165,7 @@ const Login = () => {
             {loading ? "Logging in..." : "Login"}
           </button>
         </form>
-        <div
-          className="flex cursor-pointer gap-4 md:w-3/5 border-gray-300 border w-full text-gray-800 items-center p-3 my-5 rounded-full justify-center transition hover:bg-gray-100 duration-300"
-          onClick={handleSignInWithGoogle}
-        >
-          <img src={google} alt="Google Icon" className="w-5 h-5" />
-          <span>Sign in with Google</span>
-        </div>
+        <SignInWithGoogle />
         <div className="my-4">
           <span className="text-sm text-gray-600">
             Don&apos;t have an account?
---
+++ src/Components/SignInWithGoogle.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const SignInWithGoogle = () => {
+  const handleSignInWithGoogle = async () => {
+    setLoading(true); // Show loader for Google sign-in
+
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      const loggedInUserId = user.uid;
+      localStorage.setItem("userId", loggedInUserId);
+      console.log(user);
+      saveUser(user);
+      // navigate("/dashboard/information");
+      console.log(token);
+      navigate("/dashboard/information");
+      createUserProfile(user);
+    } catch (error) {
+      console.error(error);
+      setError(true);
+    } finally {
+      setLoading(false); // Stop loading after process
+    }
+  };
+
+  const createUserProfile = (user) => {
+    const userDocRef = doc(db, "users", user.uid);
+    console.log({
+      username: user.displayName,
+      id: user.uid,
+      email: user.email,
+    });
+    const userProfileData = {
+      username: username,
+      id: user.uid,
+      email: user.email,
+    };
+
+    setDoc(userDocRef, userProfileData)
+      .then(() => {
+        return true;
+      })
+      .catch((error) => {
+        console.error("Error Creating user Profile", error);
+      });
+  };
+
+  return (
+    <div
+      className="flex cursor-pointer gap-4 md:w-3/5 border-gray-300 border w-full text-gray-800 items-center p-3 my-5 rounded-full justify-center transition hover:bg-gray-100 duration-300"
+      onClick={handleSignInWithGoogle}
+    >
+      <img src={google} alt="Google Icon" className="w-5 h-5" />
+      <span>Sign in with Google</span>
+    </div>
+  );
+};
+
+export default SignInWithGoogle;
